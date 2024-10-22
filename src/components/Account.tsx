1d372fd58f2542f42888060abdc44f34b8d9cd30
nikhilbhatia08 <bhatianikhil551@gmail.com>
2024-10-22 09:25:23 +0530

    Accounts page creation

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import SideBarForAccount from './SideBarForAccount'
+import { VscAccount } from "react-icons/vsc";
 
 function Account() {
   return (
-    <div>Account</div>
+    <>
+        <div className='flex bg-gray-100 p-8'>
+            <SideBarForAccount />
+            <div className='bg-white h-[90vh] mt-[-4px] w-[80vw] border border-gray-300 rounded-md'>
+                <div className='border-b p-2 flex items-center'>
+                    <h1 className='text-gray-500'>My Account / </h1>
+                    <div className='flex items-center'>
+                        <VscAccount className='ml-2' size={18}/>
+                        <h1 className='text-black ml-2 font-light'>Profile</h1>
+                    </div>
+                </div>
+                <div className='flex-col items-center mt-10 justify-center'>
+                    <div className='flex items-center justify-center'>
+                        <img src={require('../icons/bonnie.png')} alt="" className='rounded-full h-[15vh]'/>
+                    </div>
+                    <h1 className='mt-4 text-2xl flex items-center justify-center font-light'>Joe Rogan</h1>
+                    <h1 className='text-gray-500 flex items-center justify-center'>joe.example@gmail.com</h1>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
